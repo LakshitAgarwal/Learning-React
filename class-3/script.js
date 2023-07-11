36e8128a97d15546e03e2ddc3fc979131058d81a
Lakshit Agarwal <lakshitcollege@gmail.com>
2023-07-11 11:59:47 +0530

    Day-3 of Learning React

---
+++ class-3/script.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// This is JSX Code:
+const heading = <h1 className="title">hello world</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("container"));
+// A react element is rendered like this:
+// root.render(heading);
+
+
+// Functional Component:
+
+const Title = function () {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+      consequuntur.
+    </p>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* like this we can use ReactElement inside Functional Component.  */}
+      {heading}
+      <h2>Namaste Everyone!</h2>
+      <h2 id="functComp">Hey! This is a functional component</h2>
+      {/* Like this we can use functional Component inside another functional Component */}
+      {Title()}
+      {/* We can also do it like this: <Title /> */}
+    </div>
+  );
+};
+// Functional component is rendered like this:
+root.render(<HeaderComponent />);
